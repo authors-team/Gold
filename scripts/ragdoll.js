4eeb82f00e59066396ca0ae62e0a2424cc327287
Ryan Berkey <ryan@rybotron.com>
2019-03-12 23:53:09 -0400

    ragdoll hardcode cleanup

--- scripts/ragdoll.js
+++ scripts/ragdoll.js
@@ -1,240 +1,59 @@
 class Ragdoll {
-  constructor( lib, x, y, scale, options ){
-    this._library = lib;
+  constructor( bodyLib, x, y, scale, options ){
+    this._bodyparts     = bodyLib.bodyparts;
+    this._constraints   = bodyLib.constraints;
+
     this._scale = scale = typeof scale === 'undefined' ? 1 : scale;
+    
     let bodies = [];
+    let constraints = [];
 
-    let headOptions = Common.extend({
-      label: 'head',
-      collisionFilter: {
-        group: Body.nextGroup(true)
-      }
-    }, options);
+    //////  Create Body Parts   //////
+    for (let i = 0; i < this._bodyparts.length; i++ ) {
+      let bodypart = this._bodyparts[i];
+      let opts = Common.extend( bodypart.options, options );
+      // console.log( bodypart.options.label );
 
-    let chestOptions = Common.extend({
-      label: 'chest',
-      collisionFilter: {
-        group: Body.nextGroup(true)
-      }
-    }, options);
 
-    let leftUpperArmOptions = Common.extend({
-      label:'left-upper-arm',
-      collisionFilter: {
-        group: Body.nextGroup(true)
-      }
-    }, options);
-
-    let leftLowerArmOptions = Common.extend({}, leftUpperArmOptions, {
-      label: 'left-lower-arm'
-    });
-
-    let rightUpperArmOptions = Common.extend({
-      label:'right-upper-arm',
-      collisionFilter: {
-        group: Body.nextGroup(true)
-      }
-    }, options);
-
-    let rightLowerArmOptions = Common.extend({}, rightUpperArmOptions, {
-      label: 'right-lower-arm'
-    });
-
-    let leftUpperLegOptions = Common.extend({
-        label: 'left-upper-leg',
-        collisionFilter: {
-            group: Body.nextGroup(true)
-        }
-    }, options);
-
-    let leftLowerLegOptions = Common.extend({}, leftUpperLegOptions, {
-      label:'left-lower-leg'
-    });
-
-    let rightUpperLegOptions = Common.extend({
-        label: 'right-upper-leg',
-        collisionFilter: {
-            group: Body.nextGroup(true)
-        }
-    }, options);
-
-    let rightLowerLegOptions = Common.extend({}, rightUpperLegOptions, {
-      label: 'right-lower-leg'
-    });
-
-    for (let i = 0; i < this._library.length; i++ ) {
-      let lib = this._library[i];
-      let opts = {
-        label: lib.label
-      }
-      let body = Bodies.rectangle( x + this._scale * lib.x, y + this._scale * lib.y,  this._scale * lib.xSize,  this._scale * lib.ySize, opts );
+      let body = Bodies.rectangle( x + this._scale * bodypart.x, y + this._scale * bodypart.y,  this._scale * bodypart.xSize,  this._scale * bodypart.ySize, opts );
       bodies.push( body );
+      console.log( bodies[i].collisionFilter );
     }
 
-    // let chestToRightUpperArm = Constraint.create({
-    //     bodyA: chest,
-    //     pointA: {
-    //         x: 24 * scale,
-    //         y: -23 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -8 * scale
-    //     },
-    //     bodyB: rightUpperArm,
-    //     stiffness: 0.6
-    // });
+    //////  Create Constraints   //////
 
-    // let chestToLeftUpperArm = Constraint.create({
-    //     bodyA: chest,
-    //     pointA: {
-    //         x: -24 * scale,
-    //         y: -23 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -8 * scale
-    //     },
-    //     bodyB: leftUpperArm,
-    //     stiffness: 0.6
-    // });
+    function searchBodyLabels( nameKey, myArray ){
+        for (let i = 0; i < myArray.length; i++) {
+            if ( myArray[i].label === nameKey ) {
+                return myArray[i];
+            }
+        }
+    }
 
-    // let chestToLeftUpperLeg = Constraint.create({
-    //     bodyA: chest,
-    //     pointA: {
-    //         x: -10 * scale,
-    //         y: 30 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -10 * scale
-    //     },
-    //     bodyB: leftUpperLeg,
-    //     stiffness: 0.6
-    // });
+    for (let i = 0; i < this._constraints.length; i++ ) {
+        let options = this._constraints[i];
 
-    // let chestToRightUpperLeg = Constraint.create({
-    //     bodyA: chest,
-    //     pointA: {
-    //         x: 10 * scale,
-    //         y: 30 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -10 * scale
-    //     },
-    //     bodyB: rightUpperLeg,
-    //     stiffness: 0.6
-    // });
+        options.bodyA = searchBodyLabels( options.bodyA, bodies );
+        options.bodyB = searchBodyLabels( options.bodyB, bodies );
+        constraints.push(Constraint.create( options ));
+    }
 
-    // let upperToLowerRightArm = Constraint.create({
-    //     bodyA: rightUpperArm,
-    //     bodyB: rightLowerArm,
-    //     pointA: {
-    //         x: 0,
-    //         y: 15 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -25 * scale
-    //     },
-    //     stiffness: 0.6
-    // });
-
-    // let upperToLowerLeftArm = Constraint.create({
-    //     bodyA: leftUpperArm,
-    //     bodyB: leftLowerArm,
-    //     pointA: {
-    //         x: 0,
-    //         y: 15 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -25 * scale
-    //     },
-    //     stiffness: 0.6
-    // });
-
-    // let upperToLowerLeftLeg = Constraint.create({
-    //     bodyA: leftUpperLeg,
-    //     bodyB: leftLowerLeg,
-    //     pointA: {
-    //         x: 0,
-    //         y: 20 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -20 * scale
-    //     },
-    //     stiffness: 0.6
-    // });
-
-    // let upperToLowerRightLeg = Constraint.create({
-    //     bodyA: rightUpperLeg,
-    //     bodyB: rightLowerLeg,
-    //     pointA: {
-    //         x: 0,
-    //         y: 20 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -20 * scale
-    //     },
-    //     stiffness: 0.6
-    // });
-
-    // let headContraint = Constraint.create({
-    //     bodyA: head,
-    //     pointA: {
-    //         x: 0,
-    //         y: 25 * scale
-    //     },
-    //     pointB: {
-    //         x: 0,
-    //         y: -35 * scale
-    //     },
-    //     bodyB: chest,
-    //     stiffness: 0.6
-    // });
-
-    // let legToLeg = Constraint.create({
-    //     bodyA: leftLowerLeg,
-    //     bodyB: rightLowerLeg,
-    //     stiffness: 0.01
-    // });
-
-
-    // this._person = Composite.create({
-    //     bodies: [
-    //         head, chest, 
-    //         leftUpperArm, leftLowerArm,
-    //         rightUpperArm, rightLowerArm,
-    //         leftUpperLeg, leftLowerLeg, 
-    //         rightUpperLeg, rightLowerLeg
-    //     ],
-    //     constraints: [
-    //         upperToLowerLeftArm, upperToLowerRightArm, chestToLeftUpperArm, 
-    //         chestToRightUpperArm, headContraint, upperToLowerLeftLeg, 
-    //         upperToLowerRightLeg, chestToLeftUpperLeg, chestToRightUpperLeg,
-    //         legToLeg
-    //     ]
-    // });
-
-   this._person = Composite.create({
+    //////  Create Composite   //////
+    this._person = Composite.create({
         bodies: bodies,
-        constraints: []
+        constraints: constraints
     });
 
     World.add( world, this._person );
 
-  }
+    }
 
   show(){
     let bod = this._person.bodies;
     for( let i = 0; i < bod.length; i++  ){
       let pos = this._person.bodies[i].position;
       let angle = this._person.bodies[i].angle;
-      let lib = this._library[i];
+      let bodypart = this._bodyparts[i];
       push();
         translate( pos.x, pos.y );
         rotate(angle);
@@ -242,7 +61,7 @@ class Ragdoll {
         strokeWeight(1);
         stroke(255);
         fill(127);
-        rect(0, 0, this._scale * lib.xSize, this._scale *  lib.ySize);
+        rect(0, 0, this._scale * bodypart.xSize, this._scale *  bodypart.ySize);
       pop();        
     }
   }
--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
@@ -19,69 +18,98 @@ var coin;
 var radius = 20;
 var ragdoll;
 
-var ragdollLibrary = [
-    {
-      label: 'head',
-      x: 0,
-      y: -60,
-      xSize: 34,
-      ySize: 40,
-    },{
-      label: 'chest',
-      x: 0,
-      y: 0, 
-      xSize: 55,
-      ySize: 80
-    },{
-      label: 'rightUpperArm:',
-      x: 39,
-      y: -15, 
-      xSize: 20,
-      ySize: 40      
-    },{
-      label: 'rightLowerArm:',
-      x: 39,
-      y: 25, 
-      xSize: 20,
-      ySize: 60 
-    },{
-      label: 'leftUpperArm:',
-      x: -39,
-      y: -15, 
-      xSize: 20,
-      ySize: 40       
-    },{
-      label: 'rightLowerArm:',
-      x: -39,
-      y: 25, 
-      xSize: 20,
-      ySize: 60      
-    },{
-      label: 'leftUpperLeg',
-      x: -20,
-      y: 57, 
-      xSize: 20,
-      ySize: 40  
-    },{
-      label: 'leftLowerLeg:',
-      x: -20,
-      y: 97, 
-      xSize: 20,
-      ySize: 60  
-    },{
-      label: 'rightUpperLeg',
-      x: 20,
-      y: 57, 
-      xSize: 20,
-      ySize: 40       
-    },{
-      label: 'rightLowerLeg:',
-      x: 20,
-      y: 97, 
-      xSize: 20,
-      ySize: 60  
-    }
-  ];
+var ragdollLib = {
+  bodyparts:    [
+                  { x: 0, y: -65, xSize: 34,  ySize: 40, 
+                    options:  {
+                                label: 'head',
+                                isStatic: false, 
+                                chamfer: { radius: 15 },
+                                collisionFilter: { group: 1 }
+                              } 
+                  },
+                  { x: 0, y: 0, xSize: 55,  ySize: 80, 
+                    options:  {
+                                label: 'chest',
+                                restitution: 0.9, 
+                                chamfer: { radius: 25 },
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: -45, y: -15, xSize: 20, ySize: 40, 
+                    options:  {
+                                label: 'leftUpperArm',
+                                restitution: 0.9,   
+                                chamfer: { radius: 10 },
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: -45, y: 40, xSize: 20, ySize: 60, 
+                    options:  { 
+                                label: 'leftLowerArm',
+                                restitution: 0.9,
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: 45, y: -15, xSize: 20, ySize: 40,
+                    options:  {
+                                label: 'rightUpperArm',
+                                restitution: 0.9,  
+                                chamfer: { radius: 10 },
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: 45, y: 40, xSize: 20, ySize: 60, 
+                    options:  { 
+                                label: 'rightLowerArm',
+                                restitution: 0.9,
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: -15, y: 65, xSize: 20, ySize: 40, 
+                    options:  {
+                                label: 'leftUpperLeg',
+                                restitution: 0.9,  
+                                chamfer: { radius: 10 },
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: -20, y: 120, xSize: 20, ySize: 60,
+                    options:  { 
+                                label: 'leftLowerLeg',
+                                restitution: 0.9,
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: 15, y: 65, xSize: 20, ySize: 40, 
+                    options:  {
+                                label: 'rightUpperLeg',
+                                restitution: 0.9, 
+                                chamfer: { radius: 10 },
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                  { x: 20, y: 120, xSize: 20, ySize: 60,
+                    options:  { 
+                                label: 'rightLowerLeg',
+                                restitution: 0.9,
+                                collisionFilter: { group: -1 }
+                              } 
+                  },
+                ],
+  constraints:  [
+                  { label: 'headToChest', bodyA: 'head', bodyB: 'chest', pointA: { x: 0, y: 25 }, pointB: { x: 0, y: -35 }, stiffness: 0.8 },
+                  { label: 'chestToLeftUpperArm', bodyA: 'chest', bodyB: 'leftUpperArm', pointA: { x: -24, y: -23 }, pointB: { x: 0, y: -8 }, stiffness: 0.8 },
+                  { label: 'leftUpperToLowerArm', bodyA: 'leftUpperArm', bodyB: 'leftLowerArm', pointA: { x: 0, y: 15 }, pointB: { x: 0, y: -25 }, stiffness: 0.8 },
+                  { label: 'chestToRightUpperArm', bodyA: 'chest', bodyB: 'rightUpperArm', pointA: { x: 24, y: -23 }, pointB: { x: 0, y: -8 }, stiffness: 0.8 },
+                  { label: 'rightUpperToLowerArm', bodyA: 'rightUpperArm', bodyB: 'rightLowerArm', pointA: { x: 0, y: 15 }, pointB: { x: 0, y: -25 }, stiffness: 0.8},
+                  { label: 'chestToLeftUpperLeg', bodyA: 'chest', bodyB: 'leftUpperLeg', pointA: { x: -10, y: 30 }, pointB: { x: 0, y: -10 }, stiffness: 0.8 },
+                  { label: 'leftUpperToLowerLeg', bodyA: 'leftUpperLeg', bodyB: 'leftLowerLeg', pointA: { x: 0, y: 20 }, pointB: { x: 0, y: -20 }, stiffness: 0.8 },
+                  { label: 'chestToRightUpperLeg', bodyA: 'chest', bodyB: 'rightUpperLeg',  pointA: { x: 10, y: 30 }, pointB: { x: 0, y: -10 }, stiffness: 0.8 },
+                  { label: 'rightUpperToLowerLeg', bodyA: 'rightUpperLeg', bodyB: 'rightLowerLeg', pointA: { x: 0, y: 20 }, pointB: { x: 0, y: -20 }, stiffness: 0.8 },
+                  { label: 'legToLeg', bodyA: 'leftLowerLeg', bodyB: 'rightLowerLeg', pointA: { x: 0, y:0 }, pointB: { x: 0, y:0 }, stiffness: 0.01 }
+                ]
+};
 
 function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
@@ -108,7 +136,7 @@ function setup() {
   // mouseConstraint.mouse.element.removeEventListener("mousewheel", mouseConstraint.mouse.mousewheel);
   // mouseConstraint.mouse.element.removeEventListener("DOMMouseScroll", mouseConstraint.mouse.mousewheel);
 
-  ragdoll = new Ragdoll(ragdollLibrary, width / 2, height / 2, 1 );
+  ragdoll = new Ragdoll( ragdollLib, width / 2, height / 2, 1 );
   // coin = new Coin( width / 2, 100, radius );
 
   var options = {
